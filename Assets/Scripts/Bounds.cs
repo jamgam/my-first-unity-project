355b6f281b0681ccc541c9245da4aa1094e97971
Bryan Liang <bryanliang1995@gmail.com>
2021-02-22 14:52:17 -0800

    Directional jumping

---
+++ Assets/Scripts/Bounds.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    public CompositeCollider2D platformCollider2D;
+    public CompositeCollider2D myCollider2D;
+    public BoxCollider2D myBoxCollider2D;
+    public float yPadding = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // myBoxCollider2D.size.Set(platformCollider2D.bounds.size.x, platformCollider2D.bounds.size.y + yPadding);
+        myBoxCollider2D.size = new Vector2(platformCollider2D.bounds.size.x, platformCollider2D.bounds.size.y + yPadding);
+        myBoxCollider2D.offset = new Vector2(platformCollider2D.bounds.center.x, platformCollider2D.bounds.center.y + yPadding / 2);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,26 +3,23 @@
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
-    public Rigidbody2D rb;
-
     public float jumpForce = 20f;
-    public Transform feet;
+    public Rigidbody2D rb;
     public LayerMask groundLayers;
-
     public Animator anim;
-
     public Transform playerGraphics;
-
+    public CircleCollider2D playerCircleCollider2D;
     [HideInInspector] public bool isFacingRight = true;
 
     float mx;
+    private int airJumps = 1;
 
     // Update is called once per frame
     void Update()
     {
         mx = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGround())
+        if (Input.GetButtonDown("Jump") && airJumps > 0)
         {
             Jump();
         }
@@ -54,25 +51,32 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movement = new Vector2(mx * movementSpeed, rb.velocity.y);
 
-        rb.velocity = movement;
+        // rb.velocity = movement;
     }
 
     void Jump()
     {
-        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.GetComponent<Transform>().position;
 
-        rb.velocity = movement;
+        difference.Set(difference.x, difference.y, 0f);
+        difference.Normalize();
+        difference *= jumpForce;
+
+        rb.velocity = new Vector2(difference.x, difference.y);
+        airJumps--;
     }
 
     public bool IsGround()
     {
-        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 0.2f, groundLayers);
+        float extraGroundCheckDistance = 0.2f;
 
-        if (groundCheck != null)
+        RaycastHit2D raycastHit2D = Physics2D.CircleCast(playerCircleCollider2D.bounds.center, playerCircleCollider2D.radius, Vector2.down, extraGroundCheckDistance, groundLayers);
+
+        if (raycastHit2D.collider != null)
         {
+            airJumps = 1;
             return true;
         }
         return false;
-
     }
 }
